5a1aa6e115b5e8361af351025069b841e284d37a
Pedrohswd <pedrohsfwd@gmail.com>
2024-06-02 15:04:56 -0300

    first commit

---
+++ src/test/java/com/atividade/pedro/AtividadeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade.pedro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
